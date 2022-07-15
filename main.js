10b9bf85c1a0ccbba23a2297f08a580a4f8267da
Sfami <tomufamadi@gmail.com>
2022-07-16 00:26:06 +0200

    API Testing files added.

---
+++ main.js
@@ -0,0 +1,34 @@
+function fetchData(){
+    fetch("signs.json", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            console.log(response)
+            if(!response.ok){
+                throw Error("Error");
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+
+            const html = data
+                .map(sign => {
+                    return `
+                    <h1>${sign.name}</h1>
+                    <img src=${sign.image}></img>
+                    <p>${sign.description}</p>
+                    `;
+                })
+                .join("");
+            document.querySelector("#app").insertAdjacentHTML("afterend", html); 
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
+
+fetchData();
